aebbf0cdfbaafd61c47230c6c1067c9814783df0
Rayne Guinta <rguinta@uccs.edu>
2025-09-23 01:20:27 +0000

    task4

---
+++ homework1/tests/test_task5.py
@@ -0,0 +1,13 @@
+def fav_books():
+    books = [
+        "A Tale of Two Cities by Charles Dickens", 
+        "The Alchemest by Paulo Coelho", 
+        "Harry Potter by J. K. Rowling",
+        "Diary of a Wimpy Kid by Jeff Kinney",
+        "Green Eggs and Hamm by Dr. Seuss"]
+
+     print(books[:3])
+
+if __name__ == "__main__":
+    fav_books()
+
