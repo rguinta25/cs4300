f027fb122f4c9c3e66265eecb6d6754522a67068
Rayne Guinta <rguinta@uccs.edu>
2025-10-06 01:10:37 +0000

    3.3

--- homework2/homework2/bookings/models.py
+++ homework2/homework2/bookings/models.py
@@ -22,7 +22,7 @@ class Booking(models.Model):
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
     seat = models.ForeignKey(Seat, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    booking_date = models.DateTimeField(auto_now_add=True)
+    booking_date = models.DurationField()
 
     def __str__(self):
         return f"{self.user.username} booked {self.seat.seat_number} for {self.movie.title}"
