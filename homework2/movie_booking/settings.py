4061ad3860ed0b7dceaa0ca15413a8cbbbf03e6f
Rayne Guinta <rguinta@uccs.edu>
2025-10-07 00:16:02 +0000

    render

--- homework2/movie_booking/settings.py
+++ homework2/movie_booking/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
     'app-rguinta-19.devedu.io',  # DevEdu
-    'your-app-name.onrender.com'  # Render
+    '.onrender.com'  # Render
     ]
 
 CSRF_TRUSTED_ORIGINS = [
